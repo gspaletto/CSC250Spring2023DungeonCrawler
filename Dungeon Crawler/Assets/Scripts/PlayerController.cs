b4a5d8a4df70618b25df7b89055e92398dfb9f02
gspaletto <129095591+gspaletto@users.noreply.github.com>
2023-05-10 19:37:49 -0500

    Homework 10

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -8,27 +8,48 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
+    private bool isMoving = false;
     
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
-        print(MasterData.count);
         MasterData md = new MasterData();
+        this.isMoving = false;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Exit"))
+        {
+            SceneManager.LoadScene("DungeonRoom");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow) && !isMoving)
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && !isMoving)
         {
-            MasterData.count++;
-            SceneManager.LoadScene("DungeonRoom");
+            this.rb.AddForce(this.westExit.transform.position * movementSpeed);
+            this.isMoving = true;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !isMoving)
+        {
+            this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
+            this.isMoving = true;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !isMoving)
+        {
+            this.rb.AddForce(this.southExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
     }
 }
